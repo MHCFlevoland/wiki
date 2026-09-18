077e0cafe7f0ee2489ed5a03a298638b4c6dfd6a
Justin Klerk <justin@klerk.nl>
2026-09-18 12:59:57 +0200

    hockey food

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,92 +1,96 @@
 const { description } = require("../../package");
 
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: "Wiki MHCFlevoland",
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
-  description: description,
+	/**
+	 * Ref：https://v1.vuepress.vuejs.org/config/#title
+	 */
+	title: "Wiki MHCFlevoland",
+	/**
+	 * Ref：https://v1.vuepress.vuejs.org/config/#description
+	 */
+	description: description,
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
-  head: [
-    ["meta", { name: "theme-color", content: "#3eaf7c" }],
-    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-    [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-    ],
-  ],
+	/**
+	 * Extra tags to be injected to the page HTML `<head>`
+	 *
+	 * ref：https://v1.vuepress.vuejs.org/config/#head
+	 */
+	head: [
+		["meta", { name: "theme-color", content: "#3eaf7c" }],
+		["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+		[
+			"meta",
+			{ name: "apple-mobile-web-app-status-bar-style", content: "black" },
+		],
+	],
 
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
-  themeConfig: {
-    repo: "https://github.com/MHCFlevoland/wiki",
-    editLinks: true,
-    docsDir: "/src",
-    editLinkText: "Pas pagina aan!",
-    lastUpdated: false,
-    nav: [
-      {
-        text: "Website",
-        link: "/website/",
-      },
-      {
-        text: "ClubApp",
-        link: "/clubapp/",
-      },
-      {
-        text: "Intern",
-        link: "/Intern/",
-      },
-      {
-        text: "MHCFlevoland",
-        link: "https://MHCFlevoland.nl/",
-      },
-    ],
-    sidebar: {
-      "/clubapp/": [
-        {
-          title: "ClubApp",
-          collapsable: true,
-          children: ["", "account", "faq"],
-        },
-      ],
-      "/website/": [
-        {
-          title: "Website",
-          collapsable: true,
-          children: [""],
-        },
-      ],
-      "/intern/": [
-        {
-          title: "Interne Documentatie",
-          collapsable: true,
-          children: [""],
-        },
-      ],
-      "/intern/mail/": [
-        {
-          title: "Mail",
-          collapsable: true,
-          children: [""],
-        },
-      ],
-    },
-  },
+	/**
+	 * Theme configuration, here is the default theme configuration for VuePress.
+	 *
+	 * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+	 */
+	themeConfig: {
+		repo: "https://github.com/MHCFlevoland/wiki",
+		editLinks: true,
+		docsDir: "/src",
+		editLinkText: "Pas pagina aan!",
+		lastUpdated: false,
+		nav: [
+			{
+				text: "Website",
+				link: "/website/",
+			},
+			{
+				text: "ClubApp",
+				link: "/clubapp/",
+			},
+			{
+				text: "HockeyFood",
+				link: "/hockeyfood/",
+			},
+			{
+				text: "Intern",
+				link: "/Intern/",
+			},
+			{
+				text: "MHCFlevoland",
+				link: "https://MHCFlevoland.nl/",
+			},
+		],
+		sidebar: {
+			"/clubapp/": [
+				{
+					title: "ClubApp",
+					collapsable: true,
+					children: ["", "account", "faq"],
+				},
+			],
+			"/website/": [
+				{
+					title: "Website",
+					collapsable: true,
+					children: [""],
+				},
+			],
+			"/intern/": [
+				{
+					title: "Interne Documentatie",
+					collapsable: true,
+					children: [""],
+				},
+			],
+			"/intern/mail/": [
+				{
+					title: "Mail",
+					collapsable: true,
+					children: [""],
+				},
+			],
+		},
+	},
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+	/**
+	 * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+	 */
+	plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
 };
---
+++ src/hockeyfood/README.md
@@ -0,0 +1,68 @@
+# HockeyFood
+
+HockeyFood is een app voor trainers en coaches met materiaal voor de dagelijkse begeleiding van teams. In de app vind je onder andere oefeningen, vaardigheden, artikelen, e-books, cursussen, leerlijnen en **HockeyMind**. HockeyMind kan gebruikt worden voor vragen over hockey en kent ook de beschikbare leerlijnen en trainingsweken.
+
+## Belangrijk voor MHC Flevoland
+
+> **Wij loggen bij MHC Flevoland niet in via LISA.**  
+> Trainers en coaches loggen in met hun **e-mailadres**.
+
+De inloggegevens zijn per e-mail naar de trainers en coaches verstuurd. Gebruik het **e-mailadres dat bij MHC Flevoland bekend is**.
+
+Heb je geen e-mail ontvangen of lukt het inloggen niet? Neem dan contact op met ICT:
+
+- E-mail: [ict@mhcflevoland.nl](mailto:ict@mhcflevoland.nl)
+- WhatsApp / telefoon: [0321 315 004](tel:0321315004)
+
+## HockeyFood installeren
+
+HockeyFood is beschikbaar voor iOS en Android.
+
+- **iPhone / iPad:** [Download op Appstore](https://apps.apple.com/nl/app/hockeyfood/id6751440772)
+- **Android:** [Download op PlayStore](https://play.google.com/store/apps/details?id=com.sideline.hockeyfood)
+
+Na het installeren van de app kun je inloggen met de gegevens die je per e-mail hebt ontvangen.
+
+> Let op: de algemene instructies van HockeyFood noemen op sommige plaatsen inloggen via LISA. **Deze instructie geldt niet voor MHC Flevoland. Gebruik de per e-mail ontvangen inloggegevens.**
+
+## Wat kun je met HockeyFood?
+
+### Trainingsweken en leerlijnen
+
+Voor trainers staan trainingsweken en leerlijnen klaar. Hiermee kun je trainingen voorbereiden en aansluiten bij de ontwikkeling van het team.
+
+### Oefeningen
+
+In de bibliotheek kun je oefeningen zoeken en deze direct gebruiken tijdens een training.
+
+### Vaardigheden
+
+Vaardigheden kunnen met beeld worden uitgelegd, zodat je spelers kunt laten zien hoe een techniek of beweging uitgevoerd wordt.
+
+### HockeyMind
+
+Met HockeyMind kun je vragen stellen over hockey. HockeyMind heeft daarbij kennis van de beschikbare leerlijnen en trainingsweken.
+
+### Artikelen, e-books en cursussen
+
+Naast praktische oefeningen bevat HockeyFood materiaal waarmee trainers en coaches zichzelf verder kunnen ontwikkelen, waaronder artikelen, e-books en cursussen.
+
+## Problemen met inloggen
+
+Controleer bij problemen eerst het volgende:
+
+1. Gebruik je het e-mailadres dat bij MHC Flevoland bekend is?
+2. Gebruik je de inloggegevens uit de e-mail die je als trainer of coach hebt ontvangen?
+3. Probeer je niet via je LISA-account in te loggen?
+
+Kom je er daarna nog niet uit, neem dan contact op met ICT via [ict@mhcflevoland.nl](mailto:ict@mhcflevoland.nl) of WhatsApp/telefoon via [0321 315 004](tel:0321315004).
+
+## Meer informatie
+
+Meer uitleg en materiaal over HockeyFood is te vinden op:
+
+[start.hockeyfood.nl](https://start.hockeyfood.nl/)
+
+---
+
+**Voor MHC Flevoland is de belangrijkste afwijking van de algemene HockeyFood-handleiding dat trainers en coaches niet via LISA inloggen, maar met de inloggegevens die per e-mail zijn verstrekt.**
